7adc35d0076c824626ad00bb03ca7564377829c8
lmottasin <lmottasin@gmail.com>
2023-08-09 22:15:28 +0600

    feat: tailwind install

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
