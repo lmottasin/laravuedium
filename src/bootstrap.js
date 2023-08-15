5706e369aad0b9bf8d27977ca488b540771725fe
lmottasin <lmottasin@gmail.com>
2023-08-15 19:42:22 +0600

    feat: unauthorized access, refresh page token set in header auto

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
+import { useAuth } from '@/store/auth'
 
 window.axios = axios
-
 /* We set X-Requested-With header to tell the server it is an XHR request,
  and it serves an additional purpose so the server must consent to CORS policies */
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
@@ -10,3 +10,23 @@ window.axios.defaults.withCredentials = true
 /* so we can omit full URLs in our requests and just type in 
 the relative path of the server's API endpoint. */
 window.axios.defaults.baseURL = 'http://localhost:8000/api/v1'
+
+/* If unauthorized response occured */
+window.axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      const auth = useAuth()
+      auth.destroyTokenAndRedirectTo('register')
+    }
+
+    return Promise.reject(error)
+  }
+)
+
+// every time refresh the page
+if (localStorage.getItem('access_token')) {
+  window.axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem(
+    'access_token'
+  )}`
+}
